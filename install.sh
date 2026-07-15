71f3a1a38aec9b4d4c2bd0b45a5ecbaa56076b33
Dave Kirk <62944013+kirkdave@users.noreply.github.com>
2026-07-15 01:17:32 -0700

    Install and initial config of Ghostty

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+install() {
+	clear
+	echo
+
+	# Usage:
+	# section "Section heading..."
+	section() {
+		echo -e "\n===> $1"
+	}
+
+	section "Sudo permission required for setup..."
+	sudo echo "Sudo granted"
+
+	# We use Homebrew for package management. If it's not there, install it
+	if ! command -v brew &> /dev/null; then
+		section "Installing Homebrew..."
+		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+		eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+		brew install --yes git
+	fi
+
+	section "Installing packages..."
+	/opt/homebrew/bin/brew bundle install --file ./brewfile.packages
+
+	section "Installing applicaitons..."
+	/opt/homebrew/bin/brew bundle install --file ./brewfile.applicaitons
+
+	section "Configuring applications & packages..."
+	mkdir -p "$HOME/.config"
+	cp -rf "./config/"* "$HOME/.config/"
+	for dir in "./config"/*/; do
+		echo "$(basename "$dir")"
+	done
+}
+
+# Use a function call to prevent brew installs from stealing stdin
+install
